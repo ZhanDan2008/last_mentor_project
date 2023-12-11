20412643e807cfd1041829e8667fe18704b36709
Анна Рачкова <annarachkova@MacBook-Air-Anna.local>
2023-12-11 18:52:08 +0600

    frst

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,7 +8,29 @@ const ProductList = () => {
   }, []);
 
   console.log(products, "products");
-  return <div>ProductList</div>;
+  return (
+    <div className="container mx-auto mt-8">
+      <h2 className="text-2xl font-bold mb-4 text-purple-700">Products</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+        {products.map((product) => (
+          <div key={product.id} className="bg-purple-800 p-6 rounded-lg w-full">
+            <img
+              className="w-full h-64 object-cover mb-4 rounded"
+              src={product.image}
+              alt={product.title}
+            />
+            <p className="text-2xl font-bold mb-2 text-purple-300">
+              {product.title}
+            </p>
+            <p className="text-gray-400 mb-4">{product.description}</p>
+            <p className="text-green-500 font-bold text-lg">
+              {product.price} USD
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
